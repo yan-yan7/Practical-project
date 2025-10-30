ab4eea03c955d10e38f7a542fb5ccc2b4afbc67e
yan-yan7 <2685537227@qq.com>
2025-10-30 23:44:33 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -5,6 +5,10 @@
 3.游戏界面
 开发流程：用户界面入手
 
+加载背景
+渲染
+找开源支持png透明图
+
 
 
 //创建游戏窗口
@@ -13,30 +17,77 @@
 
 #include<stdio.h>
 #include<graphics.h>//基于“esayX”
+#include"tools.h"
 
-#define WIN_WIDTH 1012;
-#define WIN_HEIGHT 396;
+#define WIN_WIDTH 1012
+#define WIN_HEIGHT 396
+
+IMAGE  imgBgs[3];//背景图片
+int i;
+int bgX[3];//背景坐标
+int speed[3] = { 1,3,6 };//滚动速度
+
 
 //初始化
 void init()
 {
+
+
 	//创建游戏窗口
 	initgraph(WIN_WIDTH, WIN_HEIGHT);
+
+	char name[100];
+	for(int i=0;i<3;i++)
+	{
+		sprintf(name, "res/bg%03d.png", i + 1);
+		loadimage(&imgBgs[i], name);
+		bgX[i] = 0;
+		
+	}
+	
 }
+
 
 
 //渲染游戏背景
 void updateBg()
 {
+	
+	putimagePNG2(bgX[0], 0, &imgBgs[0]);
+	putimagePNG2(bgX[1], 119, &imgBgs[1]);
+	putimagePNG2(bgX[2], 330, &imgBgs[2]);
+		
+}
+
+
+//滚动
+void fly()
+{
+	for (i = 0; i < 3;i++)
+	{
+		bgX[i] -= speed[i];
+
+		if (bgX[i] < -WIN_WIDTH)
+		{
+			bgX[i] = 0;
+		}
+	}
 
 }
 
 int main(void)
 {
 	init();
-
-
-	updateBg();//
-
+	while (1) 
+	{
+		BeginBatchDraw();
+		updateBg();//
+		EndBatchDraw();
+		fly();
+		Sleep(30);//休眠
+	 }
+		
+	
+		system("pause");
 	return 0;
 }
